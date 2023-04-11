c20e5e6899538f42cf631a700878bb37f93fcbd5
mahernandez7 <miguel.hernandez-mu@uniminuto.edu.co>
2023-04-11 10:57:00 -0500

    fundamentos spring

---
+++ src/main/java/com/fundamentos/springboot/fundamenos/FundamenosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.fundamenos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamenosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamenosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentos/springboot/fundamenos/FundamenosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.fundamenos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamenosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
