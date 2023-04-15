953e0b2de3f6765b1708c77d82a71bfa353fc1ba
mahernandez7 <miguel.hernandez-mu@uniminuto.edu.co>
2023-04-15 14:35:07 -0500

    fundamentos spring

--- src/main/java/com/fundamentos/springboot/fundamenos/FundamenosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamenos/FundamenosApplication.java
@@ -74,20 +74,42 @@ public class FundamenosApplication implements CommandLineRunner {
 		userRepository.findAndSort("user", Sort.by("id").descending()).stream()
 				.forEach(user -> LOGGER.info("Ususario con metodo Sort"+user));
 
+		userRepository.findByName("John").stream()
+				.forEach(user -> LOGGER.info("Ususario con query method "+user));
+
+		LOGGER.info("Ususario con querimethod " + userRepository.findByEmailAndName("daniela@domain.com","daniela")
+				.orElseThrow(() -> new RuntimeException("usuario no encontrado")));
+
+		//Busqueda de lista por query method de LIKE
+		userRepository.findByNameLike("%user%")
+				.stream()
+				.forEach(user -> LOGGER.info("Usuario con LIKE"+ user));
+
+		//Busqueda de lista por query method de Or
+		userRepository.findByNameOrEmail(null, "daniela@domain.com")
+				.stream()
+				.forEach(user -> LOGGER.info("Usuario con Busqueda Or "+ user));
+
+		userRepository.findByBirthDateBetween(
+				LocalDate.of(2021,3,1), LocalDate.of(2021, 07, 29))
+				.stream().forEach(user -> LOGGER.info("Busqueda por fecha: "+user));
+
+		userRepository.findByNameLikeOrderByIdDesc("%user%")
+				.stream().forEach(user -> LOGGER.info("usuario por name y orden por id "+user));
 	}
 
 
 	private  void saveUsersInDataBase(){
-		User user1 = new User("John", "jhon@domain.com", LocalDate.of(2021,03,20));
-		User user2 = new User("Julie", "julie@domain.com", LocalDate.of(2021,05,21));
-		User user3 = new User("daniela", "daniela@domain.com", LocalDate.of(2021,07,04));
-		User user4 = new User("user4", "user4@domain.com", LocalDate.of(2021,07,22));
+		User user1 = new User("John", "jhon@domain.com", LocalDate.of(2021,3,20));
+		User user2 = new User("Julie", "julie@domain.com", LocalDate.of(2021,5,21));
+		User user3 = new User("daniela", "daniela@domain.com", LocalDate.of(2021,7,4));
+		User user4 = new User("user4", "user4@domain.com", LocalDate.of(2021,7,22));
 		User user5 = new User("user5", "user5@domain.com", LocalDate.of(2021,11,24));
 		User user6 = new User("user6", "user6@domain.com", LocalDate.of(2021,2,12));
 		User user7 = new User("user7", "user7@domain.com", LocalDate.of(2021,3,11));
 		User user8 = new User("user8", "user8@domain.com", LocalDate.of(2021,5,10));
 		User user9 = new User("user9", "user9@domain.com", LocalDate.of(2021,8,24));
-		User user10 = new User("user10", "user10@domain.com", LocalDate.of(2021,1,06));
+		User user10 = new User("user10", "user10@domain.com", LocalDate.of(2021,1,6));
 		List<User> list = Arrays.asList(user1, user2,user3,user4,user5,user6,user7,user8,user9,user10);
 		list.stream().forEach(userRepository::save);
 
--- src/main/java/com/fundamentos/springboot/fundamenos/repository/UserRepository.java
+++ src/main/java/com/fundamentos/springboot/fundamenos/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.fundamentos.springboot.fundamenos.repository;
 
 import com.fundamentos.springboot.fundamenos.entity.User;
 import net.bytebuddy.TypeCache;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,8 +18,24 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserEmail(String email);
 
     //Sort es una implementacion de ordenamiento de data
+
     @Query("SELECT u from User u WHERE u.name like ?1%")
     List<User> findAndSort(String name, Sort sort);
 
+    //Query Methods
+    List<User> findByName(String name);
 
+    Optional<User> findByEmailAndName(String email, String name);
+
+    //Query Methods con Like
+    List<User> findByNameLike(String name);
+    //Query Methods con Or
+    List<User> findByNameOrEmail(String name, String email);
+
+    //Query methods con busqueda por fecha
+    List<User> findByBirthDateBetween(LocalDate begin, LocalDate end);
+    //Query methods con busqueda por nombre orden por id
+    //Tambien se puede con Containing cambiando el nombre a findByNameContainingOrderByIdDesc
+    //con lo que se evida traer en el metodo like las designaciones %%
+    List<User> findByNameLikeOrderByIdDesc(String name);
 }
